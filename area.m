c2e0c816c04159bcf399cc900dc46a51138612d8
Edin <physedo@users.noreply.github.com>
2019-11-01 10:10:25 +0100

    Create area.m

---
+++ area.m
@@ -0,0 +1,10 @@
+
+disp("Rektangelsarea"); %% Visar Rektangels Area på Skärmen%%
+
+b=input('basen: ')   %%% Längden på basen på Rektangeln%%
+
+h=input('höjden: ')  %%% Höjden på Rektangeln %%%
+
+
+Area=b*h  %%% Räknar ut Rektangelns Area%%
+
