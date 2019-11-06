076985fc8b5477bf726563100e5a4a2600becb1b
Edin <physedo@users.noreply.github.com>
2019-11-06 14:29:47 +0100

    Create tal2.m

---
+++ tal2.m
@@ -0,0 +1,15 @@
+%%% Vilket tal är störst %%%
+
+function Tal3(a,b)  %%% a är första talet och b är andra talet %%%
+
+if a > b   %%% Om a är större än b ==> Första talet är störst %%%
+    
+disp('Det första talet är störst!')
+
+elseif a < b %%% Om a är mindre än b ==> Andra talet är störst %%%
+    
+disp('Det andra talet är störst!')
+
+else   %%% Annars är a=b lika stora %%%
+disp('Talen är lika stora!')
+end
