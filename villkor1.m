226482e53cdc8a76abc7dfc1b2d69ac2563a8d24
Edin <physedo@users.noreply.github.com>
2019-11-06 14:39:25 +0100

    Create villkor1.m

---
+++ villkor1.m
@@ -0,0 +1,13 @@
+%%% Två tal %%%
+
+a =input('Tal1:');   %%% Tal 1 %%%
+b =input('Tal2:');   %%% Tal 2 %%%
+
+
+if  a < b %%% Om Tal 1 är mindre än Tal 2 beräknar den c %%%
+c = 2*a   
+elseif a == b  %%% Om Tal 1 är mindre än Tal 2 beräknar den d %%%
+d= a+b
+else   %%% Om ovanstående villkor inte uppfylls så skrivs det ut att f=0  %%%
+f = 0
+end
