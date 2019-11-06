46f0efe50c9ac73b46ae5636235bf83db52cea7d
Edin <physedo@users.noreply.github.com>
2019-11-06 14:30:55 +0100

    Create talfoljd.m

---
+++ talfoljd.m
@@ -0,0 +1,12 @@
+%%%%%%%%% Talföljder %%%%%%%%
+
+
+
+figur=input('Skriv till vilken figur:'); %%% Skriv in
+
+
+for n=1:figur  %%% Med for loop skiver den ut upp till en viss figur %%%
+disp(n)
+Figur=4*n-3  
+ 
+end
