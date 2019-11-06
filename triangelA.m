125f8a34411d1e3cbccd3cfeb3e46c25d081154b
Edin <physedo@users.noreply.github.com>
2019-11-06 14:46:12 +0100

    Create triangelA.m

---
+++ triangelA.m
@@ -0,0 +1,9 @@
+%%% Beräknar triangelns Area %%%
+
+
+disp('Arean av en triangel'); %%% Skriver ut på skärmen vad den räknar ut %%%
+
+b=input('Basen:');   %%% Skriver in basen %%% 
+h=input('Höjden:');  %%% Skriver in höjden %%%
+
+Areatriangel=(b.*h)./2  %%% Räknar ut Arean av triangel %%%
