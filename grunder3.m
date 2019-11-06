dc8aded3449685b1e555b2701f3287a877e885a3
Edin <physedo@users.noreply.github.com>
2019-11-06 14:24:24 +0100

    Create grunder3.m

---
+++ grunder3.m
@@ -0,0 +1,28 @@
+disp('Addition, Subtraktion, Multiplikation och Division')
+
+%% Dina Tal%%%
+
+a=input('Tal 1:')
+
+b=input('Tal 2:')
+
+%%Beräkningar %%%
+
+%%%%% Addition%%%%
+
+Addition=b+a 
+
+
+%%%%% Subtraktion%%%%
+
+Subtraktion=b-a 
+
+
+%%%Multiplikation%%
+
+Multiplikation=a.*b
+
+
+%%%Division%%
+
+Division=a./b
