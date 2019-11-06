96db72391b7cdd3fc1db119798cd353bd5f1b2de
Edin <physedo@users.noreply.github.com>
2019-11-06 14:38:00 +0100

    Create taljfold2.m

---
+++ taljfold2.m
@@ -0,0 +1,12 @@
+%%%%%%%%% Talföljder %%%%%%%%
+
+
+n=1   %%% Börjar med figur 1 %%%
+
+while n < 10  %%% Med for loop skiver den ut upp till en viss figur %%%
+
+ n=5*n+3   %%% Skriver ut våra talföljder %%%
+ 
+ 
+ 
+ end
