f0aa151b84164544322b0b493954a6a77edb2b7f
Edin <physedo@users.noreply.github.com>
2019-11-06 14:40:53 +0100

    Create cirkel.m

---
+++ cirkel.m
@@ -0,0 +1,10 @@
+%%% Beräknar cirkelns Area %%%
+
+
+disp('Arean av en cirkel'); %%% Skriver ut på skärmen vad den räknar ut %%%
+
+r=input('radien:');   %%% Skriver in radien %%% 
+
+
+Acirkel=pi*r*r  %%% Räknar ut Arean av en cirkel %%%
+print(Acirkel)
