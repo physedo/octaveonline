a28bbb1bcbaafc19548fd3441ecf8f3bd1ba30a4
Edin <physedo@users.noreply.github.com>
2019-11-06 14:45:39 +0100

    Create rektangelA.m

---
+++ rektangelA.m
@@ -0,0 +1,9 @@
+%%% Beräknar rektangelns Area %%%
+
+
+disp('Arean av en rektangel'); %%% Skriver ut på skärmen vad den räknar ut %%%
+
+b=input('Basen:');   %%% Skriver in basen %%% 
+a=input('Höjden:');  %%% Skriver in höjden %%%
+
+Arekt=b*a  %%% Räknar ut Arean av rektangeln %%%
