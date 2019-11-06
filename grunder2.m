206896e218c2e2e365ae1f1bdd5bf8ad10f0e7ea
Edin <physedo@users.noreply.github.com>
2019-11-06 14:23:45 +0100

    Create grunder2.m

---
+++ grunder2.m
@@ -0,0 +1,15 @@
+%%%% Variabler %%%%
+
+Zlatan=189;  %%%% Zlatans Längd i cm %%%
+Messi=168;   %%%% Messis Längd i cm %%%
+
+disp(Zlatan) %%% Visa variabeln Zlatans Langd på skärmen %%%
+disp(Messi) %%% Visa variabeln Messis Langd på skärmen %%%
+
+Skillnad=Zlatan-Messi %%% Visar skillnad i längd mellan Zlatan och Messi %%%
+
+
+Treganger=3*Zlatan  %%%% Tre gånger Zlatans Längd %%%%
+
+%%%% OBS Spara först innan du kör koden %%%
+%%%%%% Tryck på Run för att köra koden %%%
