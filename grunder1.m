06b9f253ad4ca322c63df71bd0ec7ef119a573ad
Edin <physedo@users.noreply.github.com>
2019-11-06 14:23:11 +0100

    Create grunder1.m

---
+++ grunder1.m
@@ -0,0 +1,15 @@
+%%% De fyra Räknesätten %%%
+
+addition=5+5  %%% Addition %%%
+
+subtraktion=10-3 %%% Subtraktion %%%
+
+division=15/3 %%% Division %%%
+
+Multiplikation=12*3 %%% Multiplikation %%%
+
+%%% OBS Spara först innan du kör koden %%%
+%%% Tryck på Run för att köra koden %%%
+
+
+
