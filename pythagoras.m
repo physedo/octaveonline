2e31d6e1911082bbb3a8d9afbc06653850262d55
Edin <physedo@users.noreply.github.com>
2019-11-06 14:44:51 +0100

    Create pythagoras.m

---
+++ pythagoras.m
@@ -0,0 +1,14 @@
+%%%%%% Pythagoras sats %%%%%%%%%%
+
+%%%% c^2=b^2+a^2 %%%%%
+
+
+%%%% Kommando Pc(a,b) %%%%
+
+hypotenusa=Pc(5,5)  %%% Beräknar hypotenusa c %%
+
+%%%% Kommando Pa(c,b) %%%%
+katetA=Pa(15,5)  %%% Beräknar katet a %%%
+
+%%%% Kommando Pa(c,a) %%%%
+katetB=Pb(5,3)  %%% Beräknar katet b %%%
