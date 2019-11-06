573808cb448c73078fce987861f0a088c851afea
Edin <physedo@users.noreply.github.com>
2019-11-06 14:24:56 +0100

    Create grunder4.m

---
+++ grunder4.m
@@ -0,0 +1,29 @@
+%%%% Inmatning(input) av tal och villkor %%%%
+
+%%%%%%% Gissa ett Tal %%%%%%%
+
+a=input('Tal:'); %%%% Inmatning av ett Tal som har variablen a %%%%
+
+%%%% if villkor: [ om talet a är mindre än 5 så skrivs det ut på skärmen att Talet är för litet] %%%%
+
+if  a < 5      
+    
+disp('Talet är för litet')
+
+%%%% elseif villkor: [ om talet a är större än 6 så skrivs det ut på skärmen att Talet är för stort] %%%%
+
+elseif a > 6       
+    
+disp('Talet är för stort')
+
+ %%%% else villkor: [ Sriver att du har gissat rätt ] %%%%
+ 
+else              
+    
+disp('Du hade rätt')
+    
+
+end
+
+%%%% OBS Spara först innan du kör koden %%%%
+%%%% Tryck på Run för att köra koden %%%%
